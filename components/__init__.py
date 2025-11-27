9eb7d5f7a736c95c742b667642e2580385c3495e
gtmaihackathon <vipin.kumar@productsup.com>
2025-11-27 12:40:40 +0100

    Update __init__.py

--- components/__init__.py
+++ components/__init__.py
@@ -2,16 +2,23 @@
 UI Components for Content Intelligence Engine
 """
 
-from .upload_section import render_upload_section
-from .analysis_dashboard import render_analysis_dashboard
-from .gap_matrix import render_gap_matrix
-from .persona_chat import render_persona_chat
-from .strategy_view import render_strategy_view
+from .upload_section import render_upload_section, render_persona_upload
+from .analysis_dashboard import render_analysis_dashboard, render_quick_stats
+from .gap_matrix import render_gap_matrix, render_stage_analysis, render_persona_analysis
+from .persona_chat import render_persona_chat, render_comparison_tool, render_content_advisor
+from .strategy_view import render_strategy_view, render_content_brief_generator
 
 __all__ = [
     "render_upload_section",
+    "render_persona_upload",
     "render_analysis_dashboard",
+    "render_quick_stats",
     "render_gap_matrix",
+    "render_stage_analysis",
+    "render_persona_analysis",
     "render_persona_chat",
-    "render_strategy_view"
+    "render_comparison_tool",
+    "render_content_advisor",
+    "render_strategy_view",
+    "render_content_brief_generator"
 ]
